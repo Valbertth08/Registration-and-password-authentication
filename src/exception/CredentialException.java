c0cfc5a928c347fc91383b29ecd8929c1ce2f7cc
Valbertth Alves <valbertthestudos@gmail.com>
2023-08-12 22:50:57 -0300

    inicializando

---
+++ src/exception/CredentialException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class CredentialException  extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public CredentialException(String msg) {
+		super(msg);
+	}
+	
+}
